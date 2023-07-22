535cbbdae76d324e45fb228ea47383e65ea28858
Asa-to <sethuna03@gmail.com>
2023-07-22 12:09:35 +0900

    プラマイが逆

--- app/components/PaymentCard/create.tsx
+++ app/components/PaymentCard/create.tsx
@@ -16,11 +16,12 @@ import { useState } from "react";
 type Props = {
   payments: Payment[];
   users: User[];
+  initialValue?: Payment;
   close: () => void;
 };
 
 export const PaymentCreate: FC<Props> = (props) => {
-  const { payments, users, close } = props;
+  const { payments, users, close, initialValue } = props;
   const [categories, setCategories] = useState(
     Array.from(new Set(payments.map((item) => item.category)))
   );
@@ -35,8 +36,9 @@ export const PaymentCreate: FC<Props> = (props) => {
         <DateInput
           label="支払日"
           name="payDate"
+          valueFormat="YYYY年MM月DD日"
           required
-          defaultValue={new Date()}
+          defaultValue={initialValue?.payDate ?? new Date()}
         />
         <Grid>
           <Grid.Col span="auto">
@@ -50,13 +52,18 @@ export const PaymentCreate: FC<Props> = (props) => {
                 return query;
               }}
               required
-              defaultValue={categories[0]}
+              defaultValue={initialValue?.category ?? categories[0]}
               creatable
               searchable
             />
           </Grid.Col>
           <Grid.Col span="content">
-            <Radio.Group label="収支選択" name="type" required defaultValue="0">
+            <Radio.Group
+              label="収支選択"
+              name="type"
+              required
+              defaultValue={(initialValue?.value ?? 0) < 0 ? "0" : "1"}
+            >
               <Group>
                 <Radio value="0" label="支出" />
                 <Radio value="1" label="収入" />
@@ -68,11 +75,21 @@ export const PaymentCreate: FC<Props> = (props) => {
           name="userId"
           data={usersForSelect}
           label="支払い者"
-          defaultValue={usersForSelect[0].value}
+          defaultValue={initialValue?.userId ?? usersForSelect[0].value}
           required
         />
-        <TextInput label="収支" type="number" name="value" required />
-        <TextInput label="備考" name="remarks" />
+        <TextInput
+          label="収支"
+          type="number"
+          name="value"
+          defaultValue={initialValue?.value}
+          required
+        />
+        <TextInput
+          label="備考"
+          name="remarks"
+          defaultValue={initialValue?.remarks ?? ""}
+        />
         <Stack sx={{ flexDirection: "row" }}>
           <Button type="reset">キャンセル</Button>
           <Button type="submit">決定</Button>
