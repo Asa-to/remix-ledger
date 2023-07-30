b95a732a3bd76924aab0e42d305b068c3ec39b7a
Asa-to <sethuna03@gmail.com>
2023-07-30 13:19:42 +0900

    最低限の装備を整えた

--- app/routes/_layout._index.tsx
+++ app/routes/_layout._index.tsx
@@ -1,8 +1,18 @@
-import { Outlet } from "@remix-run/react";
+import { Button, Stack } from "@mantine/core";
+import { Link } from "@remix-run/react";
 import type { FC } from "react";
 
 const App: FC = () => {
-  return <Outlet />;
+  return (
+    <Stack>
+      <Button component={Link} to="/payment">
+        入出金へ
+      </Button>
+      <Button component={Link} to="/user">
+        ユーザー管理へ
+      </Button>
+    </Stack>
+  );
 };
 
 export default App;
