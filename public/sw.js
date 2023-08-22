1659568db4f5968e13b03882edf0cd98d5272af1
Asa-to <sethuna03@gmail.com>
2023-08-22 21:15:38 +0900

    a

--- public/sw.js
+++ public/sw.js
@@ -1,3 +1,12 @@
-self.addEventListener("fetch", (event) => {
-  return;
+const le = true;
+if (le) console.log("Service Worker Start");
+
+self.addEventListener("install", function (event) {
+  if (le) console.log("Service Worker Install", event);
 });
+
+self.addEventListener("activate", function (event) {
+  if (le) console.log("Service Worker Activate", event);
+});
+
+if (le) console.log("Service Worker End");
