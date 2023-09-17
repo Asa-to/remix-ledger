ceb4a5104e0979307a3eabf7ae5497bf02f69802
Asa-to <sethuna03@gmail.com>
2023-09-17 21:47:58 +0900

    l

--- app/routes/_layout.payment.edit.$id.tsx
+++ app/routes/_layout.payment.edit.$id.tsx
@@ -6,6 +6,7 @@ import {
   Radio,
   Select,
   Stack,
+  Text,
   TextInput,
 } from "@mantine/core";
 import { DateInput } from "@mantine/dates";
@@ -65,9 +66,11 @@ export const PaymentCreate = () => {
   const goBack = () => navigate(-1);
   const location = useLocation();
   const redirectTo = location.state?.from ?? "/payment";
+  const d = new Date().getTime();
 
   return (
     <Form method="POST">
+      <Text>{d}</Text>
       <Stack spacing={8}>
         <input name="redirectTo" defaultValue={redirectTo} hidden />
         <Input display="none" name="id" defaultValue={payment.id} />
