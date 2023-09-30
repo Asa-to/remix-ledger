447a3b27a50abcd8b0d29e14ae950ff44228dc41
Asa-to <sethuna03@gmail.com>
2023-09-30 22:03:33 +0900

    afda

--- app/utils/csv/generateCSV.ts
+++ app/utils/csv/generateCSV.ts
@@ -3,7 +3,7 @@ export const generateCSV = <
 >(
   data: T
 ) => {
-  const keys = data ? Object.keys(data[0]) : ["N/A"];
+  const keys = data.length === 0 ? ["N/A"] : Object.keys(data[0]);
   const csvContent =
     "data:text/csv;charset=utf-8," +
     keys.join(",") +
