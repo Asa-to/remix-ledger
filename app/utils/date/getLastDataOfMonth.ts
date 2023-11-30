67df1c4aebb72e8dfe7895b026ea0b1c242b1e5f
Asa-to <sethuna03@gmail.com>
2023-11-30 22:30:01 +0900

    asfda

--- app/utils/date/getLastDataOfMonth.ts
+++ app/utils/date/getLastDataOfMonth.ts
@@ -1,7 +1,7 @@
 export const getLastDayOfMonth = (date: Date) => {
   const year = date.getFullYear();
   const month = date.getMonth();
-  const lastDay = new Date(year, month + 1, 0);
+  const lastDay = new Date(year, month + 1, 0, 23, 59, 59, 999);
 
   return lastDay;
 };
